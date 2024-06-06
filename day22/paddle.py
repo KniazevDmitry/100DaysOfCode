b13c598ed9cb7be2aad7ee68149440246f65ebe6
Dmitrii Kniazev <dmitriikniazev@gmail.com>
2024-06-06 20:58:35 +0200

    Add Paddle class for Pong
    A new class, Paddle, has been created in paddle.py, which handles the settings and movements of the paddles in the Pong game. This class has been implanted into main.py, with methods associated to key bindings for user interactions (moving paddles up and down).

---
+++ day22/paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+
+        def paddle_create(coordinates):
+            self.goto(coordinates)
+
+        paddle_create(position)
+
+    def move_up(self):
+        self.sety(self.ycor() + 20)
+
+    def move_down(self):
+        self.sety(self.ycor() - 20)
